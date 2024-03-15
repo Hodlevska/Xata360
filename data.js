95580f3518fe7193fd71aee7fd06c041906a5b54
Hodlevska <73657261+Hodlevska@users.noreply.github.com>
2024-03-15 17:34:26 +0200

    commit

---
+++ data.js
@@ -0,0 +1,177 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--1",
+      "name": "Двір 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5158530219924575,
+          "pitch": 0.29363826243191227,
+          "rotation": 0,
+          "target": "1--2"
+        },
+        {
+          "yaw": 0.009019051257190824,
+          "pitch": 0.12584972661299254,
+          "rotation": 0.7853981633974483,
+          "target": "3--3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--2",
+      "name": "Двір 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.11247216688558304,
+        "pitch": -0.12323507044135162,
+        "fov": 1.3360112054562112
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.158187550752265,
+          "pitch": 0.3324393367155256,
+          "rotation": 0,
+          "target": "0--1"
+        },
+        {
+          "yaw": -0.36570394649211124,
+          "pitch": 0.19056337161209846,
+          "rotation": 0,
+          "target": "3--3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--4",
+      "name": "Дім 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.21927911714917947,
+          "pitch": 0.31452885653914464,
+          "rotation": 0,
+          "target": "3--3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--3",
+      "name": "Дім 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.10286022893738433,
+          "pitch": 0.4994806669376324,
+          "rotation": 0,
+          "target": "2--4"
+        },
+        {
+          "yaw": 1.4173349797297874,
+          "pitch": 0.6050752086858431,
+          "rotation": 0,
+          "target": "1--2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Українська Хата ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
